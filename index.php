51ca8416cf475327aba252a11ea429e06da6e7f5
Thariq <toriqthalib94@gmail.com>
2023-03-07 08:01:56 +0800

    menambahkan paragraph

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 
 <h1>Hello World</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eveniet totam deleniti laborum cum tempora voluptatum reprehenderit ea, quos facilis numquam enim nisi sunt at suscipit nulla eum rem dignissimos.</p>
     
 </body>
 </html>
