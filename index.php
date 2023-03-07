761cf0a25643544ed81fbade28a000eb7e38fe6c
Thariq <toriqthalib94@gmail.com>
2023-03-07 08:06:05 +0800

    menambah list pada index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 
 <h1>Hello World</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eveniet totam deleniti laborum cum tempora voluptatum reprehenderit ea, quos facilis numquam enim nisi sunt at suscipit nulla eum rem dignissimos.</p>
-    
+    <li>opsi a</li>
+    <li>opsi b</li>    
+
 </body>
 </html>
